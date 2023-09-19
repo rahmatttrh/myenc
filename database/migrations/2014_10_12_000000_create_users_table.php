109698c8c6d4e0e3e8554fdec28b37d1cd2c3795
“rahmatttrh” <rahmattrust@gmail.com>
2023-09-19 10:59:59 +0700

    login by nik or email

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ class CreateUsersTable extends Migration
          $table->string('name');
          $table->string('email')->unique();
          $table->string('username')->unique();
-         // $table->string('nik')->unique();
          $table->timestamp('email_verified_at')->nullable();
          $table->string('password');
          $table->rememberToken();
