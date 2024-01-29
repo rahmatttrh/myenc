30885e24279f09239211bfb190b7c83d8fae1d7f
Rahmat Hidayat <rahmattrust@gmail.com>
2024-01-29 10:03:42 +0700

    fitur export pdf kpa

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -12,49 +12,49 @@ class DepartmentSeeder extends Seeder
     *
     * @return void
     */
-   public function run()
-   {
-      Department::create([
-         'name' => 'HRD',
-         'created_at' => NOW(),
-         'updated_at' => NOW()
-      ]);
+   // public function run()
+   // {
+   //    Department::create([
+   //       'name' => 'HRD',
+   //       'created_at' => NOW(),
+   //       'updated_at' => NOW()
+   //    ]);
 
-      Department::create([
-         'name' => 'IT',
-         'created_at' => NOW(),
-         'updated_at' => NOW()
-      ]);
+   //    Department::create([
+   //       'name' => 'IT',
+   //       'created_at' => NOW(),
+   //       'updated_at' => NOW()
+   //    ]);
 
-      Department::create([
-         'name' => 'Finance',
-         'created_at' => NOW(),
-         'updated_at' => NOW()
-      ]);
+   //    Department::create([
+   //       'name' => 'Finance',
+   //       'created_at' => NOW(),
+   //       'updated_at' => NOW()
+   //    ]);
 
-      Department::create([
-         'name' => 'Operation',
-         'created_at' => NOW(),
-         'updated_at' => NOW()
-      ]);
+   //    Department::create([
+   //       'name' => 'Operation',
+   //       'created_at' => NOW(),
+   //       'updated_at' => NOW()
+   //    ]);
 
-      Department::create([
-         'name' => 'Operation',
-         'created_at' => NOW(),
-         'updated_at' => NOW()
-      ]);
+   //    Department::create([
+   //       'name' => 'Operation',
+   //       'created_at' => NOW(),
+   //       'updated_at' => NOW()
+   //    ]);
 
 
-      Department::create([
-         'name' => 'QHSSE',
-         'created_at' => NOW(),
-         'updated_at' => NOW()
-      ]);
+   //    Department::create([
+   //       'name' => 'QHSSE',
+   //       'created_at' => NOW(),
+   //       'updated_at' => NOW()
+   //    ]);
 
-      Department::create([
-         'name' => 'General Service',
-         'created_at' => NOW(),
-         'updated_at' => NOW()
-      ]);
-   }
+   //    Department::create([
+   //       'name' => 'General Service',
+   //       'created_at' => NOW(),
+   //       'updated_at' => NOW()
+   //    ]);
+   // }
 }
