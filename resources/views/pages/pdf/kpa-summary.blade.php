1ca3b3fa6f386dd4c99aa6852a4fd2863bf5583a
Rahmat Hidayat <rahmattrust@gmail.com>
2024-01-29 11:15:40 +0700

    edit pdf

--- resources/views/pages/pdf/kpa-summary.blade.php
+++ resources/views/pages/pdf/kpa-summary.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app-doc')
 @section('title')
-   KPA {{$karyawan->biodata->fullName()}}-{{$semester}}-{{$tahun}}
+   KPA {{$karyawan->biodata->fullName()}} {{$semester}}-{{$tahun}}
 @endsection
 @section('content')
 <div class="container-xl">
@@ -47,7 +47,16 @@
                </div>
                <div class="col-6 text-end">
                   <p class="h3">Achievement</p> 
-                  <b><h1>{{$rating}}</h1></b>
+                  
+                  <b>
+                     <h1>
+                        {{-- <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                           <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
+                        </svg> --}}
+                      {{$rating}}
+                     </h1>
+                  </b>
                </div>
             {{-- <div class="col-12 my-4">
                   <h1>{{$schedule->vessel->name}} </h1>
