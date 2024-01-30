dae8469bb822dd74fc0fcd1cda6c73d74451eb19
Rahmat Hidayat <rahmattrust@gmail.com>
2024-01-30 11:37:39 +0700

    edit submit button only on leader

--- resources/views/pages/kpa/kpa.blade.php
+++ resources/views/pages/kpa/kpa.blade.php
@@ -186,7 +186,7 @@ KPA
                                     <td><span class="badge badge-danger badge-lg"><b>Di Reject HRD</b></span></td>
                                     @endif
                                     <td class="text-right">
-                                        @if($kpa->status == 0)
+                                        @if($kpa->status == 0 && auth()->user()->hasRole('Leader'))
                                         <button class="btn btn-sm btn-warning" data-toggle="modal" data-target="#modal-submit-{{$kpa->id}}"><i class="fas fa-rocket"></i> Submit</button>
                                         <button class="btn btn-sm btn-danger" data-toggle="modal" data-target="#modal-delete-{{$kpa->id}}"><i class="fas fa-trash"></i> Delete</button>
                                         @elseif($kpa->status == '1' || $kpa->status == '2')
