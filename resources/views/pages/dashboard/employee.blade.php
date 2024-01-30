072e3fc3d355118de73451cf400d61451839ecb8
Rahmat Hidayat <rahmattrust@gmail.com>
2024-01-30 10:12:51 +0700

    edit dashboard employee feed

--- resources/views/pages/dashboard/employee.blade.php
+++ resources/views/pages/dashboard/employee.blade.php
@@ -119,31 +119,40 @@ Dashboard
             </div>
             <div class="card-body">
                <ol class="activity-feed">
-                  <li class="feed-item feed-item-secondary">
+                  {{-- <li class="feed-item feed-item-secondary">
                      <time class="date" datetime="9-25">Sep 25</time>
                      <span class="text">Responded to need <a href="#">"Volunteer opportunity"</a></span>
-                  </li>
+                  </li> --}}
                   <li class="feed-item feed-item-success">
-                     <time class="date" datetime="9-24">Sep 24</time>
-                     <span class="text">Added an interest <a href="#">"Volunteer Activities"</a></span>
+                     <time class="date" datetime="9-24">Jan 30 09:20</time>
+                     <span class="text">Login from System
+                        {{-- <a href="#">"Volunteer Opportunity"</a> --}}
+                     </span>
                   </li>
                   <li class="feed-item feed-item-info">
-                     <time class="date" datetime="9-23">Sep 23</time>
-                     <span class="text">Joined the group <a href="single-group.php">"Boardsmanship
-                           Forum"</a></span>
+                     <time class="date" datetime="9-23">Jan 29 14:20</time>
+                     <span class="text">Logout from System
+                        {{-- <a href="#">"Volunteer Opportunity"</a> --}}
+                     </span>
                   </li>
                   <li class="feed-item feed-item-warning">
-                     <time class="date" datetime="9-21">Sep 21</time>
-                     <span class="text">Responded to need <a href="#">"In-Kind Opportunity"</a></span>
+                     <time class="date" datetime="9-21">Jan 29 11:05</time>
+                     <span class="text">Login into System
+                        {{-- <a href="#">"Volunteer Opportunity"</a> --}}
+                     </span>
                   </li>
                   <li class="feed-item feed-item-danger">
-                     <time class="date" datetime="9-18">Sep 18</time>
-                     <span class="text">Created need <a href="#">"Volunteer Opportunity"</a></span>
+                     <time class="date" datetime="9-18">Jan 29 09:15</time>
+                     <span class="text">Logout from System
+                        {{-- <a href="#">"Volunteer Opportunity"</a> --}}
+                     </span>
                   </li>
                   <li class="feed-item">
-                     <time class="date" datetime="9-17">Sep 17</time>
-                     <span class="text">Attending the event <a href="single-event.php">"Some New
-                           Event"</a></span>
+                     <time class="date" datetime="9-17">Jan 29 08:05</time>
+                     <span class="text">Login into System
+                        {{-- <a href="single-event.php">"Some New
+                           Event"</a> --}}
+                        </span>
                   </li>
                </ol>
             </div>
