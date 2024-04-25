ebcf7348e5ad776cbabb8ced5224c96c6c4bf0b1
Rahmat Hidayat <rahmattrust@gmail.com>
2024-04-25 09:00:58 +0700

    edit sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -20,7 +20,7 @@
                </a>
             </div>
          </div>
-         @if (auth()->user()->hasRole('Administrator|HRD'))
+         {{-- @if (auth()->user()->hasRole('Administrator|HRD'))
          <ul class="nav">
             <li class="nav-item">
                <a data-toggle="collapse" href="#vessel">
@@ -73,7 +73,7 @@
             </li>
 
          </ul>
-         @endif
+         @endif --}}
          <ul class="nav">
 
             <li class="nav-item {{ (request()->is('/')) ? 'active' : '' }}">
@@ -89,7 +89,7 @@
                </span>
                <h4 class="text-section">Main Menu</h4>
             </li>
-
+            
             @if (!auth()->user()->hasRole('Administrator'))
             <li class="nav-item {{ (request()->is('employee/detail/*')) ? 'active' : '' }}">
                <a href="{{route('employee.detail', [enkripRambo(auth()->user()->employee->id), enkripRambo('contract')])}}">
@@ -127,9 +127,45 @@
 
             @if (auth()->user()->hasRole('Administrator|HRD|Leader|Manager'))
             <li class="nav-item">
+               <a data-toggle="collapse" href="#vessel">
+                  <i class="fas fa-server"></i>
+                  <p>Master Data</p>
+                  <span class="caret"></span>
+               </a>
+               <div class="collapse" id="vessel">
+                  <ul class="nav nav-collapse">
+                     <li>
+                        <a href="{{route('unit')}}">
+                           <span class="sub-item">Bisnis Unit</span>
+                        </a>
+                     </li>
+                     <li>
+                        <a href="{{route('department')}}">
+                           <span class="sub-item">Department</span>
+                        </a>
+                     </li>
+                     <li>
+                        <a href="{{route('designation')}}">
+                           <span class="sub-item">Level</span>
+                        </a>
+                     </li>
+                     <li>
+                        <a href="{{route('position')}}">
+                           <span class="sub-item">Jabatan</span>
+                        </a>
+                     </li>
+                     <li>
+                        <a href="{{route('so')}}">
+                           <span class="sub-item">Struktur Organisasi</span>
+                        </a>
+                     </li>
+                  </ul>
+               </div>
+            </li>
+            <li class="nav-item">
                <a data-toggle="collapse" href="#kpi">
                   <i class="fas fa-bullseye"></i>
-                  <p>Performance Evaluation</p>
+                  <p>Performance</p>
                   <span class="caret"></span>
                </a>
                <div class="collapse" id="kpi">
@@ -163,6 +199,12 @@
                      </li>
                   </ul>
                </div>
+            </li>
+            <li class="nav-item {{ (request()->is('employee/*')) ? 'active' : '' }}">
+               <a href="{{route('employee', enkripRambo('active'))}}">
+                  <i class="fas fa-users"></i>
+                  <p>Employee</p>
+               </a>
             </li>
             @endif
          </ul>
