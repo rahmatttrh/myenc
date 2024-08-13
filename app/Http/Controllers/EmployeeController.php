3118a2f9c5acce62f5a7c6d781e3bc5bcbe685f1
Rahmat H <rahmattrust@gmail.com>
2024-08-13 13:33:02 +0700

    fix bug on server

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -172,16 +172,16 @@ class EmployeeController extends Controller
       for ($i = 0; $i < $jumlah; $i++) {
          $employee = Employee::find($arrayItem[$i]);
 
-         try {
-            $user = User::create([
-               'name' => $employee->biodata->first_name . ' ' . $employee->biodata->last_name,
-               'email' => $employee->biodata->email,
-               'username' => $employee->nik,
-               'password' => Hash::make('12345678')
-            ]);
-         } catch (Exception $e) {
-            return redirect()->back()->with('danger', 'Can not activate employee  ' . $employee->biodata->first_name . ' ' . $employee->biodata->last_name . ', Error log : ' . $e->getMessage());
-         }
+         // try {
+         //    $user = User::create([
+         //       'name' => $employee->biodata->first_name . ' ' . $employee->biodata->last_name,
+         //       'email' => $employee->biodata->email,
+         //       'username' => $employee->nik,
+         //       'password' => Hash::make('12345678')
+         //    ]);
+         // } catch (Exception $e) {
+         //    return redirect()->back()->with('danger', 'Can not activate employee  ' . $employee->biodata->first_name . ' ' . $employee->biodata->last_name . ', Error log : ' . $e->getMessage());
+         // }
 
          $employee->update([
             'status' => 1,
@@ -219,18 +219,18 @@ class EmployeeController extends Controller
          }
       }
 
-      // if (auth()->user()->hasRole('Administrator')) {
-      //    $departmentId = null;
-      // } else {
-      //    $user = Employee::find(auth()->user()->getEmployeeId());
-      //    $departmentId = $user->department_id;
-      // }
-      // Log::create([
-      //    'department_id' => $departmentId,
-      //    'user_id' => auth()->user()->id,
-      //    'action' => 'Publish',
-      //    'desc' => 'Employees Data'
-      // ]);
+      if (auth()->user()->hasRole('Administrator')) {
+         $departmentId = null;
+      } else {
+         $user = Employee::find(auth()->user()->getEmployeeId());
+         $departmentId = $user->department_id;
+      }
+      Log::create([
+         'department_id' => $departmentId,
+         'user_id' => auth()->user()->id,
+         'action' => 'Publish',
+         'desc' => 'Employees Data'
+      ]);
       return redirect()->route('employee', enkripRambo('active'))->with('success', 'Employee successfully activated');
    }
 
